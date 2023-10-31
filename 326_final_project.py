5bf0abdd04f7d2a642dd0a1ff5b94051b4730dcd
DavidZann <107975837+DavidZann@users.noreply.github.com>
2023-10-31 10:08:25 -0400

    Update 326_final_project.py

--- 326_final_project.py
+++ 326_final_project.py
@@ -3,8 +3,9 @@ Welcome to our Final Project: Currency Converter
 
 Zafir Kazi, David Zannou, Max Eliker, Andrew Bian
 """
+from forex_python.converter import CurrencyRates
 
-def currency_converter():
+def currency_converter(amount, from_currency, to_currency):
     pass
 
 def main():

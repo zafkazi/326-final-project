0299f911a9d5ed2dbaec363be635c158d082dd80
DavidZann <107975837+DavidZann@users.noreply.github.com>
2023-12-07 16:20:39 -0500

    Update 326_final_project.py

--- 326_final_project.py
+++ 326_final_project.py
@@ -4,6 +4,7 @@ Welcome to our Final Project: Currency Converter
 Zafir Kazi, David Zannou, Max Eliker, Andrew Bian
 """
 import requests
+from forex_python.converter import CurrencyRates
 
 def get_exchange_rate(api_key, base_currency, target_currency):
     url = f"https://open.er-api.com/v6/latest/{base_currency}"
@@ -24,25 +25,34 @@ def convert_currency(amount, exchange_rate):
     else:
         return None
 
-if __name__ == "__main__":
-    # Replace 'YOUR_API_KEY' with your actual API key from Open Exchange Rates
+def get_user_input():
+    amount = float(input("Enter the amount you want to convert: "))
+    from_currency = input("Enter the source currency (e.g., USD): ").upper()
+    to_currency = input("Enter the target currency (e.g., EUR): ").upper()
+    return amount, from_currency, to_currency
+
+def display_result(amount, from_currency, result, to_currency):
+    print(f"{amount} {from_currency} is equal to {result:.2f} {to_currency}")
+
+def currency_converter():
     api_key = 'YOUR_API_KEY'
-    
-    # Define the base and target currencies
     base_currency = 'USD'
     target_currency = 'EUR'
 
-    # Get the exchange rate
     exchange_rate = get_exchange_rate(api_key, base_currency, target_currency)
 
     if exchange_rate is not None:
-        # Input the amount to convert
-        amount_to_convert = float(input(f"Enter the amount in {base_currency}: "))
+        amount_to_convert, from_currency, to_currency = get_user_input()
 
-        # Perform the currency conversion
-        converted_amount = convert_currency(amount_to_convert, exchange_rate)
+        result = convert_currency(amount_to_convert, exchange_rate)
 
-        # Display the result
-        print(f"{amount_to_convert} {base_currency} is equal to {converted_amount:.2f} {target_currency}")
+        display_result(amount_to_convert, from_currency, result, to_currency)
     else:
         print("Currency conversion failed.")
+
+def main():
+    print("Welcome to the Currency Converter")
+    currency_converter()
+
+if __name__ == "__main__":
+    main()

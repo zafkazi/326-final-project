afbba32bd622daa3d852077a19323a43ec1713ee
zafkazi <zkazi02@terpmail.umd.edu>
2023-12-14 21:24:33 -0500

    Update 326_final_project.py

--- 326_final_project.py
+++ 326_final_project.py
@@ -1,16 +1,18 @@
+"""
+Welcome to our Final Project: Currency Converter
+
+Zafir Kazi, David Zannou, Max Eliker, Andrew Bian
+"""
 import requests
 from forex_python.converter import CurrencyRates
 import tkinter as tk
 from tkinter import OptionMenu, Label, Entry, Button, StringVar
 import pandas as pd
-import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime
-import tkinter.messagebox
 
-api_key = 'YOUR_API_KEY'
-matplotlib.use("TkAgg")
+api_key = '  API_KEY'
 
 def get_exchange_rate(api_key, base_currency, target_currency):
     """
@@ -64,7 +66,7 @@ def get_historical_exchange_rates(api_key, base_currency, target_currency, start
     Returns:
     DataFrame: with the expected columns and dates, otherwise returns None
     """
-    url = f"https://open.er-api.com/v6/time-series/{start_date}/{end_date}"
+    url = f"https://open.er-api.com/v6/time-series/{start_date}/{end_date}" 
     params = {"apikey": api_key, "base": base_currency, "symbols": target_currency}
 
     response = requests.get(url, params=params)
@@ -131,9 +133,6 @@ def plot_exchange_rate_over_time(api_key, base_currency, target_currency, start_
     plt.ylabel("Exchange Rate")
     plt.legend()
     plt.xticks(rotation=45)
-
-    # Save figure before showing
-    plt.savefig('exchange_rate_plot.png')
     plt.show()
 
 def rating_best_currencies(api_key):
@@ -178,19 +177,7 @@ def rating_worst_currencies(api_key):
     worst_currencies = sorted(ratings, key=ratings.get)
     return worst_currencies
 
-def plot_inflation_rate(inflation, years):
-    sns.lineplot(x=years, y=inflation)
-    plt.xlabel('Years')
-    plt.ylabel('Inflation Rate (%)')
-    plt.title('Inflation Rate Over Time')
-    plt.savefig('inflation_plot.png')  # Save the plot as an image file
-    plt.show()
-
-inflation = [3.73, 2.62, 2.35, 1.43, 1.55, 2.19, 2.4, 2.21, 1.93, 3.48, 8.27]
-years = [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
-plot_inflation_rate(inflation, years)
-
-def create_inflation_table():
+def create_inflation_table(inflation, years):
     """
     Create a line plot that displays the inflation rate for several years
 
@@ -198,27 +185,31 @@ def create_inflation_table():
     inflation: list of rates that correspond to a certain  year
     years: list of years that correspond to certain inflation rate
     """
-    inflation_data = {
-        'Year': [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022],
-        'Inflation Rate (%)': [3.73, 2.62, 2.35, 1.43, 1.55, 2.19, 2.4, 2.21, 1.93, 3.48, 8.27]
-    }
+    data = pd.DataFrame({'Years': years, 'Inflation Rate (5%) ': inflation})
+    sns.lineplot(data=data, x=years, y=inflation)
+    plt.xlabel('Years')
+    plt.ylabel('Inflation Rate (%)')
+    plt.title('Inflation Rate Over Time')
+    plt.savefig('inflation_plot.png')  # Save the plot as an image file
+    plt.show()
 
-    inflation_df = pd.DataFrame(inflation_data)
-    return inflation_df
+
+inflation = [3.73, 2.62, 2.35, 1.43, 1.55, 2.19, 2.4, 2.21, 1.93, 3.48, 8.27]
+years = [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
+create_inflation_table(inflation, years)
 
 # Tkinter GUI setup
-    """
-    Set up a Tkinter currency converter and exchange rate calculator
-    Conversion tool and plotting of graphs
+"""
+Set up a Tkinter currency converter and exchange rate calculator
+Conversion tool and plotting of graphs
 
-    GUI displays:
-    -Input fields for conversion amount, base currency, and target currency
-    -Buttons:
+GUI displays:
+-Input fields for conversion amount, base currency, and target currency
+-Buttons:
     -inflation rate table
     -rating best and worst currencies
     -conversion of currencies and plotting against rate of exchange
-    """
-
+"""
 app = tk.Tk()
 app.title("Currency Converter and Exchange Rate Plotter")
 
@@ -273,9 +264,9 @@ inflation_table_button.grid(row=9, column=0, columnspan=2, pady=10)
 # Function to show the result in a messagebox or any other desired way
 def show_result(result):
     if result is not None:
-        tk.messagebox.showinfo("Result", str(result))
+        result_label.config(text=str(result))
     else:
-        tk.messagebox.showerror("Error", "Operation failed.")
+        result_label.config(text="Operation failed.")
 
 # Run the Tkinter main loop
 app.mainloop()
